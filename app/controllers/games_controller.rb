78c0bbb09f86bde904d9117f10791b1e8f644ecf
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2022-12-15 18:53:49 +0100

    rails new

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,11 @@
 class GamesController < ApplicationController
   def new
-    array = ('a'..'z').to_a
-    @letters = []
-    array.each {|letter|  } array.sample(10)
+    # array = ('a'..'z').to_a
+    # @letters = []
+    # array.each do { |letter| array.sample(10)
+    #  }
   end
-  # def score
-  # end
+
+  def score
+  end
 end
